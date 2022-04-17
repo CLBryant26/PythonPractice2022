759cc7c237405cc19e4291a033176af5dfcf7da5
Crystal Bryant <98703730+CLBryant26@users.noreply.github.com>
2022-04-16 19:17:49 -0500

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+# Allow user to input their First Name and assign that input to a variable
+print ("Crystal 2")
+
+# Allow user to input their Last Name and assign that input to a variable
+print ("Bryant 7")
+
+# Print a welcome message that includes the user's First Name and Last Name using the variables.
+print ("Hello, Nice to meet you! Crystal Bryant 27")
