4c5d0e09e180109868b008e00d9d442c1f6b0be2
Crystal Bryant <98703730+CLBryant26@users.noreply.github.com>
2022-04-18 22:29:02 -0500

    Create users info.py

---
+++ users info.py
@@ -0,0 +1,8 @@
+# Allow user to input their First Name and assign that input to a variable
+first_name = str(input("what is your first name? "))
+
+# Allow user to input their Last Name and assign that input to a variable
+last_name = str(input("what is your last name"))
+
+# Print a welcome message that includes the user's First Name and Last Name using the variables.
+print ("Hello, Nice to meet you!" "first_name last_name")
